13b7fa7a653fb5018432527875e246c12695f109
Kylin <1159469891@qq.com>
2026-05-14 14:49:55 +0800

    feat: 添加taroify

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,15 +1,17 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.scss'
+import { View, Text } from "@tarojs/components";
+import { useLoad } from "@tarojs/taro";
+import "./index.scss";
+import { Button } from "@taroify/core";
 
-export default function Index () {
+export default function Index() {
   useLoad(() => {
-    console.log('Page loaded.')
-  })
+    console.log("Page loaded.");
+  });
 
   return (
-    <View className='index'>
+    <View className="index">
       <Text>Hello world!</Text>
+      <Button color="primary">按钮</Button>
     </View>
-  )
+  );
 }
